69d7ffa52e8ca2952cadffee56567b524d7212e1
wuqinlong <wuqinlong1129@163.com>
2015-12-02 16:02:17 +0800

    test git on mac

--- 20151202/hwafan.20151202/hwafan.20151202Tests/hwafan_20151202Tests.m
+++ 20151202/hwafan.20151202/hwafan.20151202Tests/hwafan_20151202Tests.m
@@ -27,7 +27,8 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-   
+    int a = 10;
+    XCTAssert(a==0,@"Pass");
 }
 
 - (void)testPerformanceExample {
